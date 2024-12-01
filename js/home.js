4f8ebcda05100c3e7dd2098342ad74d7fe8d4b7c
masudulalam <masuudulalam@gmail.com>
2024-12-01 09:14:32 +0600

    add money event handler is progressing

--- js/home.js
+++ js/home.js
@@ -1,1 +1,22 @@
-console.log('from home.js');
+// add money to the account
+/**
+ * s-1: add event handler
+ * prevent page reload after form submit
+ * 
+ * s-2: get money from the amount input field to be added to the available balance
+ */
+
+// step 1: add an event handler to the add money button
+document.getElementById('btn-add-money').addEventListener('click', function(event) {
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // step 2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    // get the provided pin number
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    console.log(inputPinNumber);
+
+})
